44cbd64d3659e1381dacda9b3f59c9d45738ccfd
alexij <achasch@gmail.com>
2019-01-18 21:54:01 +0300

    what type

--- script.js
+++ script.js
@@ -13,10 +13,11 @@ $(document).ready(function() {
   var bReset = $('.buttons__reset');
 
   // streams 
-  var bResetClick  = fromEvent(bReset, 'click');
+  var bIterClick  = fromEvent(bIter,  'click');
+  var bResetClick = fromEvent(bReset, 'click');
   var inputsKeyup = merge.apply(this, inputs.map(function(i,e) { return fromEvent(e, 'keyup'); }));
   
-  // functions
+  // functions helper
   var resetLabels = function() {
     labels.each(function(i,e) { $(e).text(''); });
   };
@@ -25,7 +26,18 @@ $(document).ready(function() {
     resetLabels();
   };
   
+  // functions iterate
+  var whatType = function() {
+    var checked = Array.from(radios).find(function(e) { return $(e).is(':checked') });
+    return $(checked).attr('value');
+  };
+  
+  var iterate = function() {
+    console.log(whatType());
+  };
+  
   // subscribers
   bResetClick.subscribe(resetAll);
   inputsKeyup.subscribe(resetLabels);
+  bIterClick.subscribe(iterate);
 });
