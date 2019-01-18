56c2f0d53c5ad637a9df7be0f06ce5ffbfe2edc3
alexij <achasch@gmail.com>
2019-01-18 21:32:12 +0300

    code structure

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
 
+
+$(document).ready(function() {
+  // elements
+  var inputs = $('.inputs-table__input');
+  var labels = $('.results-table__label');
+  var radios = $('.radios__radio');
+  var bIter  = $('.buttons__iterate');
+  var bReset = $('.buttons__reset');
+
+  // streams 
+  var bResetClick = rxjs.fromEvent(bReset, 'click');
+  
+  // functions
+  var resetAll = function() {
+    inputs.each(function(i,e) { $(e).val(''); });
+    labels.each(function(i,e) { $(e).text(''); });
+  };
+  
+  // subscribers
+  bResetClick.subscribe(resetAll);
 });
