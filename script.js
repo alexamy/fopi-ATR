aa295dd735d702c6f59df6171b37e1e3ff0dc1b5
alexij <achasch@gmail.com>
2019-01-18 22:06:25 +0300

    lines

--- script.js
+++ script.js
@@ -27,6 +27,13 @@ $(document).ready(function() {
   };
   
   // functions iterate
+  var lines = {
+    horizontal: [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15]],
+    vertical:   [[0,4,8,12], [1,5,9,13], [2,6,10,14], [3,7,11,15]],
+    diagonalLeft:  [[4,1], [8,5,2], [12,9,6,3], [13,10,7], [14,11]],
+    diagonalRight: [[7,2], [11,6,1], [15,10,5,0], [14,9,4], [13,8]]
+  }
+  
   var whatType = function() {
     var checked = Array.from(radios).find(function(e) { return $(e).is(':checked') });
     return $(checked).attr('value');
